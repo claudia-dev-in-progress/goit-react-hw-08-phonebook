be459152127d3b8334ecee7fe21c1a53fc12be49
claudiapapp <122099742+claudiapapp@users.noreply.github.com>
2023-10-08 17:35:10 +0000

    Deploying to gh-pages from @ claudiapapp/goit-react-hw-08-phonebook@41488b27c5d90b71f9c166a5dd0796a54df16e38 🚀

--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const itemsSelector = (state) => state.contacts.items;
-export const isLoadingSelector = (state) => state.contacts.isLoading;
-export const errorSelector = (state) => state.contacts.error;
-export const filterSelector = (state) => state.filter.value;
